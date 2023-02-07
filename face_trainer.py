97aed0ca972efce6ffc2902c608d0e8275ad928e
kyoungseo <102573192+kyounggseo@users.noreply.github.com>
2023-02-07 23:35:58 +0900

    Update face_trainer.py

--- face_trainer.py
+++ face_trainer.py
@@ -13,19 +13,25 @@ def getImagesAndLabels(path):
     imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
     faceSamples=[]
     ids = []
+    
     for imagePath in imagePaths:
         PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
         img_numpy = np.array(PIL_img,'uint8')
         id = int(os.path.split(imagePath)[-1].split(".")[1])
         faces = detector.detectMultiScale(img_numpy)
-        for (x,y,w,h) in faces:
+        
+        for (x,y,w,h) in faces: # 얼굴 규격 잡기
             faceSamples.append(img_numpy[y:y+h,x:x+w])
             ids.append(id)
     return faceSamples,ids
+
 print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
+
 faces,ids = getImagesAndLabels(path)
-recognizer.train(faces, np.array(ids))
+recognizer.train(faces, np.array(ids)) # 총 100장의 데이터 찍기
+
 # Save the model into trainer/trainer.yml
+
 recognizer.write('trainer/trainer.yml') # recognizer.save() worked on Mac, but not on Pi
 # Print the numer of faces trained and end program
 print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
