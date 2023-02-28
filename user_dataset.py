0760910279efc53eed5429249f4972e91a357ed1
kyoungseo <102573192+kyounggseo@users.noreply.github.com>
2023-02-28 22:03:07 +0900

    Update user_dataset.py

--- user_dataset.py
+++ user_dataset.py
@@ -5,14 +5,12 @@ cam = cv2.VideoCapture(0)
 cam.set(3, 640) # set video width
 cam.set(4, 480) # set video height
 
-face_detector = cv2.CascadeClassifier('/home/kyoungseo/FacialRecognitionProject/haarcascade_frontalface_default.xml')
+face_detector = cv2.CascadeClassifier('/home/kyoungseo/FacialRecognitionProject/haarcascade_frontalface_default.xml') 
 
-# For each person, enter one numeric face id
-face_id = input('\n enter user id end press <return> ==>  ')
+face_id = input('\n enter user id ==>  ') # User Id 입력
 
-print("\n [INFO] Initializing face capture. Look the camera and wait ...")
+print("\n [INFO] Initializing face capture. Look the camera and wait ... !!") 
 
-# Initialize individual sampling face count
 count = 0
 
 while(True):
@@ -24,17 +22,19 @@ while(True):
     for (x,y,w,h) in faces:
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
         count += 1
+        
         # Save the captured image into the datasets folder
-        cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+        cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w]) # jpg 형식으로 저장
         cv2.imshow('image', img)
         
-    k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+    k = cv2.waitKey(100) & 0xff # 종료
     
     if k == 27:
         break
     
     elif count >= 30: # Take 30 face sample and stop video
          break
+            
 # Do a bit of cleanup
 print("\n [INFO] Exiting Program and cleanup stuff")
 
